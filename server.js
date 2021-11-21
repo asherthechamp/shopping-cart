79724f0ea94fa2ef3a7c0c29f56271f29d049bd4
Ashenafi Bordea <ashukite@gmail.com>
2021-11-20 17:57:13 -0800

    port from env

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const shortid = require("shortid");
 const app = express();
+const config = require('dotenv').config();
 
 app.use(bodyParser.json());
 
@@ -94,5 +95,5 @@ app.post("/api/orders", async (req, res) => {
 // var mongoDB = process.env.MONGODB_URI || dev_db_url;
 // app.listen(mongoDB, () => console.log("serve at github pages"));
 
-// const port = process.env.PORT || 5000;
-// app.listen(port, () => console.log("serve at http://localhost:5000"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("serve at http://localhost:5000"));
