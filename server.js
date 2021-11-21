d12a5269d969e5e15f3f6b697b774ff858da74ce
Ashenafi <ashenafibordea@gmail.com>
2021-11-20 17:37:10 -0800

    db connection

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-const express = require("express");
 const bodyParser = require("body-parser");
+const express = require("express");
 const mongoose = require("mongoose");
 const shortid = require("shortid");
-
 const app = express();
+
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost/react-shopping-cart-db", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifieidTopology: true,
-});
+// mongoose.connect("mongodb://localhost/react-shopping-cart-db", {
 
+const user = "asherthechamp";
+const pass = "MERNdev123";
+const cls = "cluster0.bmbjt";
+const db = "shopping-cart";
+mongoose.connect(
+  `mongodb+srv://${user}:${pass}@${cls}.mongodb.net/${db}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifieidTopology: true,
+  }
+);
 const Product = mongoose.model(
   "products",
   new mongoose.Schema({
@@ -80,6 +88,11 @@ app.post("/api/orders", async (req, res) => {
   const order = await Order(req.body).save();
   res.send(order);
 });
+
+// var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+// var dev_db_url = 'mongodb+srv://asherthechamp:<password>@cluster0.s84uu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// var mongoDB = process.env.MONGODB_URI || dev_db_url;
+// app.listen(mongoDB, () => console.log("serve at github pages"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("serve at http://localhost:5000"));
